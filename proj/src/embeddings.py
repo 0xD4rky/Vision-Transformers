5219788babe9b1a728a444b782f46d57f0a9d9c1
Ishaan Kumar <ishaan.kumar281@gmail.com>
2024-08-04 11:28:47 +0530

    embeddings done

---
+++ proj/src/embeddings.py
@@ -0,0 +1,51 @@
+from base import *
+
+class PatchEmbeddings(nn.Module):
+    """
+    Convert the image into patches and then project them into a vector space.
+    """
+
+    def __init__(self, config):
+        super().__init__()
+        self.image_size = config["image_size"]
+        self.patch_size = config["patch_size"]
+        self.num_channels = config["num_channels"]
+        self.hidden_size = config["hidden_size"]
+        self.num_patches = (self.image_size // self.patch_size) ** 2
+        self.projection = nn.Conv2d(self.num_channels, self.hidden_size, kernel_size=self.patch_size, stride=self.patch_size)
+
+    def forward(self, x):
+        # {batch_size, num_channels, image_size, image_size}-> {batch_size, num_patches, vector_dim}
+        x = self.projection(x)
+        x = x.flatten(2).transpose(1, 2)
+        return x
+
+class Embeddings(nn.Module):
+    
+    """
+    adding positional information to extracted patch embeddings
+    """
+    
+    def __init__(self,config):
+        self.config = config
+        self.patch_emb = PatchEmbeddings(config)
+        
+        self.cls_token = nn.Parameter(torch.randn(1,1,config["vector_dim"]))
+        
+        # create learnable positional encoding and add +1 dim for [CLS]
+        self.positional_encoding = nn.Parameter(torch.randn(1,self.patch_emb.num_patches + 1, config["vector_dim"]))
+        self.droput = nn.Dropout(config["droput_prob"])
+        
+    def forward(self,x):
+        x = self.patch_emb(x)
+        batch_size, _, _ = x.size()
+        # expand the [cls] token to batch size
+        #{1,1,vector_dim} -> (batch_size,1,hidden_size)
+        cls_tokens = self.cls_token.expand(batch_size,-1,-1)
+        """
+        concatenating cls token to inputn sequence
+        size : {num_patches + 1}
+        """
+        x = torch.cat((cls_tokens,x),dim = 1)
+        x = x + self.positional_encoding
+        return x

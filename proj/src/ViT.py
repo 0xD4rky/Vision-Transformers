9ae7bcabd48ca9134cfb67ca7babafdf1a261e19
Ishaan Kumar <ishaan.kumar281@gmail.com>
2024-08-06 16:08:48 +0530

    classification setup

--- proj/src/ViT.py
+++ proj/src/ViT.py
@@ -1,5 +1,7 @@
 from base import *
 
+print("hi")
+
 class PatchEmbeddings(nn.Module):
     """
     Convert the image into patches and then project them into a vector space.
@@ -192,10 +194,56 @@ class Encoder(nn.Module):
         else:
             return (x, all_attentions)
 
+class Classification(nn.Module):
+    
+    """
+    ViT model for classification
+    """       
         
+    def __init__(self,config):
+        super().__init__()
+        self.config = config
+        self.img_size = config["img_size"]
+        self.vector_dim = config["vector_dim"]
+        self.num_classes = config["num_classes"]
         
-                
-            
+        # follow the below pipepline :)
+        
+        self.embeddings = Embeddings(config)
+        self.encoder = Encoder(config)
+        self.classifier = nn.Linear(self.vector_dim,self.num_classes)
+        self.apply(self._init_weights)
+        
+    def forward(self, x, output_attentions=False):
+        # Calculate the embedding output
+        embedding_output = self.embedding(x)
+        # Calculate the encoder's output
+        encoder_output, all_attentions = self.encoder(embedding_output, output_attentions=output_attentions)
+        # Calculate the logits, take the [CLS] token's output as features for classification
+        logits = self.classifier(encoder_output[:, 0, :])
+        # Return the logits and the attention probabilities (optional)
+        if not output_attentions:
+            return (logits, None)
+        else:
+            return (logits, all_attentions)
+    
+    def _init_weights(self, module):
+        if isinstance(module, (nn.Linear, nn.Conv2d)):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=self.config["initializer_range"])
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        elif isinstance(module, Embeddings):
+            module.position_embeddings.data = nn.init.trunc_normal_(
+                module.position_embeddings.data.to(torch.float32),
+                mean=0.0,
+                std=self.config["initializer_range"],
+            ).to(module.position_embeddings.dtype)
+
+
+print("clear hai boss")
             
             
         

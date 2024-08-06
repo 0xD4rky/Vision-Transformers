0d4fbe4b4d2e75f40f0468da494517b7bd2d321b
Ishaan Kumar <ishaan.kumar281@gmail.com>
2024-08-06 12:08:21 +0530

    GELU activation

--- proj/src/base.py
+++ proj/src/base.py
@@ -10,3 +10,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from PIL import image
+
+
+class NewGELUActivation(nn.Module):
+    """
+    Implementation of the GELU activation function currently in Google BERT repo (identical to OpenAI GPT). Also see
+    the Gaussian Error Linear Units paper: https://arxiv.org/abs/1606.08415
+
+    Taken from https://github.com/huggingface/transformers/blob/main/src/transformers/activations.py
+    """
+
+    def forward(self, input):
+        return 0.5 * input * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (input + 0.044715 * torch.pow(input, 3.0))))
+

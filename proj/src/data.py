90e82dd8bb4e287e28f23ca7d57c82cf5da6dd2e
Ishaan Kumar <ishaan.kumar281@gmail.com>
2024-08-21 22:43:13 +0530

    data loading done

--- proj/src/data.py
+++ proj/src/data.py
@@ -1,23 +1,44 @@
+# Import libraries
 import torch
 import torchvision
 import torchvision.transforms as transforms
 
-import torch
-from torchvision.datasets import CIFAR10
-from torchvision import transforms
-from torch.utils.data import DataLoader
 
-def get_cifar10_dataloaders(batch_size):
+def prepare_data(batch_size=4, num_workers=2, train_sample_size=None, test_sample_size=None):
+    train_transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Resize((32, 32)),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomResizedCrop((32, 32), scale=(0.8, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=train_transform)
+    if train_sample_size is not None:
+        # Randomly sample a subset of the training set
+        indices = torch.randperm(len(trainset))[:train_sample_size]
+        trainset = torch.utils.data.Subset(trainset, indices)
     
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=num_workers)
     
-    train_dataset = CIFAR10(root='./data', train=True, download=True, transform=transform)
-    test_dataset = CIFAR10(root='./data', train=False, download=True, transform=transform)
+    test_transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Resize((32, 32)),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                        download=True, transform=test_transform)
+    if test_sample_size is not None:
+        # Randomly sample a subset of the test set
+        indices = torch.randperm(len(testset))[:test_sample_size]
+        testset = torch.utils.data.Subset(testset, indices)
+    
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=num_workers)
 
-    return train_loader, test_loader
+    classes = ('plane', 'car', 'bird', 'cat',
+            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    return trainloader, testloader, classes

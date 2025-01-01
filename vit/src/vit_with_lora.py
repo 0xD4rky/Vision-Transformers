bf98fecb3fa85a4b7ec01c187bb1a9bb33c7835d
0xD4rky <ishaan.kumar281@gmail.com>
2025-01-01 22:52:44 +0530

    adding lora layer

--- vit/src/vit_with_lora.py
+++ vit/src/vit_with_lora.py
@@ -6,4 +6,4 @@ import math
 class LoRALayer(nn.Module):
     """Low-Rank Adaptation layer"""
     def __init__(self, in_features, out_features, rank=4, alpha=16):
-        super().__init__()
+        super(LoRALayer,self).__init__()

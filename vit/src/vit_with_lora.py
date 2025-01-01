f3f7bfe67a1b4763bbeee8210077e6883fe3b34c
0xD4rky <ishaan.kumar281@gmail.com>
2025-01-01 22:55:40 +0530

    building vit

--- vit/src/vit_with_lora.py
+++ vit/src/vit_with_lora.py
@@ -18,3 +18,53 @@ class LoRALayer(nn.Module):
     def forward(self, x):
         x = x @ (self.lora_A @ self.lora_B) * self.scaling
         return x
+    
+class PatchEmbeddings(nn.Module):
+    """
+    Convert the image into patches and then project them into a vector space.
+    """
+
+    def __init__(self, config):
+        super().__init__()
+        self.image_size = config["image_size"]
+        self.patch_size = config["patch_size"]
+        self.num_channels = config["num_channels"]
+        self.vector_dim = config["vector_dim"]
+        self.num_patches = (self.image_size // self.patch_size) ** 2
+        self.projection = nn.Conv2d(self.num_channels, self.vector_dim, kernel_size=self.patch_size, stride=self.patch_size)
+
+    def forward(self, x):
+        # {batch_size, num_channels, image_size, image_size}-> {batch_size, num_patches, vector_dim}
+        x = self.projection(x)
+        x = x.flatten(2).transpose(1, 2)
+        return x
+
+class Embeddings(nn.Module):
+    
+    """
+    adding positional information to extracted patch embeddings
+    """
+    
+    def __init__(self,config):
+        self.config = config
+        self.patch_emb = PatchEmbeddings(config)
+        
+        self.cls_token = nn.Parameter(torch.randn(1,1,config["vector_dim"]))
+        
+        # create learnable positional encoding and add +1 dim for [CLS]
+        self.positional_encoding = nn.Parameter(torch.randn(1,self.patch_emb.num_patches + 1, config["vector_dim"]))
+        self.droput = nn.Dropout(config["droput_prob"])
+        
+    def forward(self,x):
+        x = self.patch_emb(x)
+        batch_size, _, _ = x.size()
+        # expand the [cls] token to batch size
+        #{1,1,vector_dim} -> (batch_size,1,hidden_size)
+        cls_tokens = self.cls_token.expand(batch_size,-1,-1)
+        """
+        concatenating cls token to inputn sequence
+        size : {num_patches + 1}
+        """
+        x = torch.cat((cls_tokens,x),dim = 1)
+        x = x + self.positional_encoding
+        return x

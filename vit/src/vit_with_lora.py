d57a76bd6605212aa397ddc600291af90cc1dd8b
0xD4rky <ishaan.kumar281@gmail.com>
2025-01-01 22:54:57 +0530

    adding lora

--- vit/src/vit_with_lora.py
+++ vit/src/vit_with_lora.py
@@ -7,3 +7,14 @@ class LoRALayer(nn.Module):
     """Low-Rank Adaptation layer"""
     def __init__(self, in_features, out_features, rank=4, alpha=16):
         super(LoRALayer,self).__init__()
+        self.rank = rank
+        self.scaling = alpha / rank
+        
+        self.lora_A = nn.Parameter(torch.zeros(in_features, rank))
+        self.lora_B = nn.Parameter(torch.zeros(rank, out_features))
+        nn.init.kaiming_uniform_(self.lora_A, a=math.sqrt(5))
+        nn.init.zeros_(self.lora_B)
+
+    def forward(self, x):
+        x = x @ (self.lora_A @ self.lora_B) * self.scaling
+        return x
